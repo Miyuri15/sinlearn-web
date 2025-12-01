c947c4275b673d9bbf4d64a8ae86eb93f5770b65
Miyuri15 <miyurilokuhewage15@gmail.com>
2025-12-02 01:53:12 +0530

    Fix/ Render issue with client pages

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 "use client";
 import "@/lib/i18n";
 import Image from "next/image";
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 "use client";
 import "@/lib/i18n";
 import { useState, useEffect } from "react";
